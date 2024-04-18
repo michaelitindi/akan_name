e275a60af8f9909dd6747eaae0f34a9f91c4917e
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-04-18 09:07:26 +0300

    implement bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import Car from './Car';
+// import Car from './Car';
 // import Person from './Person';
-import Animal from './Animal';
+// import Animal from './Animal';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/styles.css";
 
-document.getElementById("btn").addEventListener("click", function(){
-    const car = new Car("Peugeot", "3008", 2020);
-    const rabit = new Animal("Bunny", 6, "Female");
-    alert(rabit.getName());
-    alert(car.getYear());
+// document.getElementById("btn").addEventListener("click", function(){
+//     const car = new Car("Peugeot", "3008", 2020);
+//     const rabit = new Animal("Bunny", 6, "Female");
+//     alert(rabit.getName());
+//     alert(car.getYear());
 
-});
+// });
 
